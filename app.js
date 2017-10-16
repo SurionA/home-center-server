9b669ef34bb6d26cc6fe13a51cecd84d3e71265b
SurionA <frere.maxime@gmail.com>
2017-10-16 10:14:19 +0200

    #ADD# init logger module

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const http = require('http');
 const fs = require('fs');
-const alert = require('./lib/alert/index');
+
 require('dotenv-safe').load({
     path: `${__dirname}/config/.env`,
     sample: `${__dirname}/config/.env.example`,
     allowEmptyValues: false,
 });
+
+const CONSTANTS = require('./lib/constants');
+const logger = require('./lib/logger/index')(CONSTANTS.loggerLabelMain);
+const alert = require('./lib/alert/index');
 
 const server = http.createServer((req, res) => {
     fs.readFile('./index.html', 'utf-8', (error, content) => {
@@ -16,6 +20,7 @@ const server = http.createServer((req, res) => {
 const io = require('socket.io').listen(server);
 
 server.listen(process.env.PORT);
+logger.info('HomeCenter Server start...');
 
 const radioFrequenceControl = require('home-center-radio-frequence-control');
 const musicControl = require('home-center-music-control');
@@ -25,7 +30,7 @@ hydrometriesArduino.initSocket(io);
 musicControl.initSocket(io);
 radioFrequenceControl.initSocket(io);
 
-const cronJobs = require('./lib/cron');
+const cronJobs = require('./lib/cron/index');
 
 cronJobs.start(hydrometriesArduino);
 
@@ -33,6 +38,7 @@ process.on('exit', () => {
     const type = 'alert';
     const content = 'process Home-center server down. Catch by event "exit"';
 
+    logger.error(content);
     alert.sms.send(type, content)
         .then(() => process.exit())
         .catch(() => process.exit());
@@ -42,6 +48,7 @@ process.on('SIGINT', () => {
     const type = 'alert';
     const content = 'process Home-center server down. Catch by event SIGINT';
 
+    logger.error(content);
     alert.sms.send(type, content)
         .then(() => process.exit())
         .catch(() => process.exit());
