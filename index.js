5f81cd1b383de6b5555ed87d94965f7c3f996ac0
SurionA <frere.maxime@gmail.com>
2017-10-30 07:50:34 +0100

    wip

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+
 const { hydrometriesSchema } = require('./schema');
 const { getHydrometries, getHydrometryById } = require('./dataloader');
 
 const { hydrometries, hydrometry } = hydrometriesSchema;
 const app = express();
 
+const helperMiddleware = [
+    bodyParser.json(),
+    bodyParser.text({ type: 'application/graphql' }),
+    (req, res, next) => {
+        if (req.is('application/graphql')) {
+            req.body = { query: req.body };
+        }
+        next();
+    },
+];
+
 app.listen('1212');
 
 app.use(
     '/graphql/hydrometries/:id',
-    graphqlHTTP(req => ({
+    ...helperMiddleware,
+    graphqlExpress(req => ({
         schema: hydrometry,
         context: {
             getHydrometry: getHydrometryById(req.params.id),
@@ -19,10 +34,12 @@ app.use(
         graphiql: true,
     }))
 );
+//app.get('/graphiql/hydrometry', graphiqlExpress({ endpointURL: '/graphql/hydrometries/:id' }));
 
 app.use(
     '/graphql/hydrometries',
-    graphqlHTTP(req => ({
+    ...helperMiddleware,
+    graphqlExpress(req => ({
         schema: hydrometries,
         context: {
             getHydrometries: getHydrometries(),
@@ -30,3 +47,4 @@ app.use(
         graphiql: true,
     }))
 );
+app.get('/graphiql/hydrometries', graphiqlExpress({ endpointURL: '/graphql/hydrometries' }));
